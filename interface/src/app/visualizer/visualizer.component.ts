df917b95d26ea602a5124fc0fa8f577616ffdcb4
Michael Gira <contact@michaelgira.me>
2017-12-22 01:57:20 -0600

    (Try) to get xeogl working

--- interface/src/app/visualizer/visualizer.component.ts
+++ interface/src/app/visualizer/visualizer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 declare const xeogl: any;
 
@@ -9,15 +9,63 @@ declare const xeogl: any;
 })
 export class VisualizerComponent implements OnInit {
 
+	@ViewChild('container') container: ElementRef;
+	@ViewChild('view') viewCanvas: ElementRef;
+
 	scene: any;
 
 	constructor() { }
 
 	ngOnInit() {
+		// setTimeout(() => {
+		// 	this.draw();
+		// }, 500);
+		setInterval(() => {
+			this.resizeCanvas();
+		}, 1000);
+	}
+
+	resizeCanvas() {
+		console.log(this.container.nativeElement);
+		this.viewCanvas.nativeElement.width  = this.container.nativeElement.innerWidth;
+		this.viewCanvas.nativeElement.height = this.container.nativeElement.innerHeight;
+	}
+
+	draw() {
+
 		console.log('create scene');
 		this.scene = new xeogl.Scene({
-			canvasId: 'visualizer'
+			// canvas: this.viewCanvas.nativeElement,
+			// canvas: 'view',
+			// transparent: true
 		});
+
+		// Allow user camera control
+		new xeogl.InputControl();
+
+		const box = new xeogl.BoxGeometry({
+			xSize: 1,
+			ySize: 1,
+			zSize: 1
+		});
+
+		const material = new xeogl.MetallicMaterial({
+			baseColor: [0.56, 0.57, 0.58],
+			metallic: 1,
+			roughness: 0.5
+		});
+
+		const entity = new xeogl.Entity({
+			geometry: box,
+			material
+		});
+
+		console.log(xeogl);
+		// console.log(xeogl, xeogl.InputControl);
+
+		// entity.scene.on('tick', () => {
+		// 	entity.camera.view.rotateEyeY(1.0);
+		// });
 	}
 
 }
