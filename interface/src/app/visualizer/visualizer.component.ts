5e4d51978c889c1b8c32ec50b6a56fc95ea48215
Michael Gira <michaelgira23@gmail.com>
2017-12-23 17:14:50 -0700

    But revert back to xeogl anyways

--- interface/src/app/visualizer/visualizer.component.ts
+++ interface/src/app/visualizer/visualizer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import * as THREE from 'three';
-// import '../common/OrbitControls';
+import * as ResizeSensor from 'css-element-queries/src/ResizeSensor';
 
+// import * as xeogl from '../common/xeogl.min';
 declare const xeogl: any;
 
 @Component({
@@ -12,11 +12,9 @@ declare const xeogl: any;
 export class VisualizerComponent implements OnInit {
 
 	@ViewChild('container') container: ElementRef;
+	@ViewChild('canvas') canvas: ElementRef;
 
-	scene: THREE.Scene;
-	camera: THREE.Camera;
-	renderer: THREE.WebGLRenderer;
-	// controls: THREE.OrbitControls;
+	scene: any;
 
 	gridWidth = 5;
 	gridHeight = 5;
@@ -24,35 +22,44 @@ export class VisualizerComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
-		const width = this.container.nativeElement.clientWidth;
-		const height = this.container.nativeElement.clientHeight;
-		this.scene = new THREE.Scene();
-		this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+		const resizeCanvas = () => {
+			const width = this.container.nativeElement.clientWidth;
+			const height = this.container.nativeElement.clientHeight;
 
-		this.renderer = new THREE.WebGLRenderer();
-		this.renderer.setSize(width, height);
-		this.container.nativeElement.appendChild(this.renderer.domElement);
-
-		const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-		const cube = new THREE.Mesh(geometry, material);
-		this.scene.add(cube);
-
-		this.camera.position.z = 5;
-
-		// this.controls = new THREE.OrbitControls(this.camera);
-		// this.controls.target.set(0, 0, 0);
-
-		const animate = () => {
-			requestAnimationFrame(animate);
-
-			cube.rotation.x += 0.1;
-			cube.rotation.y += 0.1;
-
-			this.renderer.render(this.scene, this.camera);
+			this.canvas.nativeElement.width = width;
+			this.canvas.nativeElement.height = height;
 		};
+		new ResizeSensor(this.container.nativeElement, resizeCanvas);
 
-		animate();
+		console.log('xeo', xeogl);
+
+		this.scene = new xeogl.Scene({
+			canvas: this.canvas.nativeElement
+		});
+
+		// Allow user camera control
+		new xeogl.InputControl();
+
+		const geometry = new xeogl.BoxGeometry({
+			xSize: 1,
+			ySize: 1,
+			zSize: 1
+		});
+
+		const material = new xeogl.MetallicMaterial({
+			baseColor: [0.56, 0.57, 0.58],
+			metallic: 1,
+			roughness: 0.5
+		});
+
+		const entity = new xeogl.Entity({
+			geometry,
+			material
+		});
+
+		entity.scene.on('tick', () => {
+			entity.camera.view.rotateEyeY(1.0);
+		});
 	}
 
 	// resizeCanvas() {
@@ -78,16 +85,16 @@ export class VisualizerComponent implements OnInit {
 	// 		zSize: 1
 	// 	});
 	//
-	// 	const material = new xeogl.MetallicMaterial({
-	// 		baseColor: [0.56, 0.57, 0.58],
-	// 		metallic: 1,
-	// 		roughness: 0.5
-	// 	});
+		// const material = new xeogl.MetallicMaterial({
+		// 	baseColor: [0.56, 0.57, 0.58],
+		// 	metallic: 1,
+		// 	roughness: 0.5
+		// });
 	//
-	// 	const entity = new xeogl.Entity({
-	// 		geometry: box,
-	// 		material
-	// 	});
+		// const entity = new xeogl.Entity({
+		// 	geometry: box,
+		// 	material
+		// });
 	//
 	// 	console.log(xeogl);
 	// 	// console.log(xeogl, xeogl.InputControl);
