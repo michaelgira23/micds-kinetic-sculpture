2bd83a788ed4053a9848c2460ce1f51a729e3df4
Michael Gira <michaelgira23@gmail.com>
2017-12-24 16:58:34 -0700

    Add multiple modules in an array

--- interface/src/app/visualizer/visualizer.component.ts
+++ interface/src/app/visualizer/visualizer.component.ts
@@ -14,10 +14,18 @@ export class VisualizerComponent implements OnInit {
 	@ViewChild('container') container: ElementRef;
 	@ViewChild('visualizer') canvas: ElementRef;
 
-	scene: any;
+	spaceBetween = 5;
 
-	gridWidth = 5;
-	gridHeight = 5;
+	gridX = 5;
+	gridY = 5;
+	moduleLength = 1;
+
+	modules: any[][] = [];
+
+	xLength = (this.gridX * this.moduleLength) + ((this.gridX - 1) * this.spaceBetween);
+	yLength = (this.gridY * this.moduleLength) + ((this.gridY - 1) * this.spaceBetween);
+
+	xeogl = xeogl;
 
 	constructor() { }
 
@@ -33,29 +41,94 @@ export class VisualizerComponent implements OnInit {
 		resizeCanvas();
 		new ResizeSensor(this.container.nativeElement, resizeCanvas);
 
+		// Create scene
 		xeogl.scene = new xeogl.Scene({
 			canvas: this.canvas.nativeElement,
-			// backgroundColor: [0.3, 0.6, 0.9]
-			// transparent: true
+			backgroundColor: 'red'
 		});
-		new xeogl.Entity({
-			geometry: new xeogl.TorusGeometry({
-				radius: 1.0,
-				tube: 0.3,
-				radialSegments: 120,
-				tubeSegments: 60
-			}),
-			material: new xeogl.MetallicMaterial({
-				baseColorMap: new xeogl.Texture({
-					src: '/assets/textures/uvGrid2.jpg'
-				})
-			})
+
+		xeogl.scene.camera.view.look = [
+			this.xLength / 2,
+			this.spaceBetween,
+			this.yLength / 2
+		];
+
+		xeogl.scene.camera.view.eye = [
+			-this.xLength,
+			(this.xLength + this.yLength) / 2,
+			-this.yLength
+		];
+
+		// Generate x and y labels
+		// console.log('xeogl', xeogl);
+		// for (let i = 0; i < this.gridX; i++) {
+		// 	new xeogl.Entity({
+		// 		geometry: new xeogl.VectorTextGeometry({
+		// 			text: i,
+		// 			origin: [0, 0, 0],
+		// 			size: 2 // Size of each square character cell
+		// 		}),
+		// 		material: new xeogl.PhongMaterial({
+		// 			emissive: [0.5, 1.0, 1.0],
+		// 			lineWidth: 2
+		// 		}),
+		// 		lights: new xeogl.Lights({
+		// 			lights: [] // No lights - rely on emissive color
+		// 		}),
+		// 		transform: new xeogl.Translate({
+		// 			// xyz: [i * this.spaceBetween, 0, -this.spaceBetween]
+		// 			xyz: [0, 40, 0]
+		// 		})
+		// 	});
+		// }
+
+		// Generate modules
+		const tipGeometry = new xeogl.BoxGeometry({
+			xSize: this.moduleLength,
+			ySize: this.moduleLength,
+			zSize: this.moduleLength
 		});
-		xeogl.scene.camera.view.eye = [0, 0, -4];
-		xeogl.scene.on('tick', function () {
-			this.camera.view.rotateEyeY(0.5);
-			// this.camera.view.rotateEyeX(0.3);
-		});
+
+		for (let i = 0; i < this.gridX; i++) {
+			this.modules[i] = [];
+
+			for (let j = 0; j < this.gridY; j++) {
+				this.modules[i][j] = new xeogl.Entity({
+					geometry: tipGeometry,
+					material: new xeogl.MetallicMaterial({
+						baseColorMap: new xeogl.Texture({
+							src: '/assets/textures/uvGrid2.jpg'
+						})
+					}),
+					transform: new xeogl.Translate({
+						xyz: [
+							i * this.spaceBetween,
+							0,
+							j * this.spaceBetween
+						]
+					})
+				});
+			}
+		}
+
+		// new xeogl.Entity({
+		// 	geometry: new xeogl.TorusGeometry({
+		// 		radius: 1.0,
+		// 		tube: 0.3,
+		// 		radialSegments: 120,
+		// 		tubeSegments: 60
+		// 	}),
+		// 	material: new xeogl.MetallicMaterial({
+		// 		baseColorMap: new xeogl.Texture({
+		// 			src: '/assets/textures/uvGrid2.jpg'
+		// 		})
+		// 	})
+		// });
+		// xeogl.scene.camera.view.eye = [0, 0, -4];
+		// xeogl.scene.on('tick', function () {
+		// 	this.camera.view.rotateEyeY(0.5);
+		// 	// this.camera.view.rotateEyeX(0.3);
+		// });
 
 		// Allow user camera control
 		new xeogl.InputControl();
