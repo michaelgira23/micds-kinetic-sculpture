6a6d9dc9a8a991019dd7f379f1e66eef33f327da
Michael Gira <michaelgira23@gmail.com>
2017-12-24 21:11:50 -0700

    Switch back to three.js because xeogl docs are lacking
    Last switch I swear

--- interface/src/app/visualizer/visualizer.component.ts
+++ interface/src/app/visualizer/visualizer.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as ResizeSensor from 'css-element-queries/src/ResizeSensor';
-
-// import * as xeogl from '../common/xeogl.min';
-declare const xeogl: any;
+import * as THREE from 'three';
+import { OrbitControls } from '@avatsaev/three-orbitcontrols-ts';
 
 @Component({
 	selector: 'app-visualizer',
@@ -25,113 +24,97 @@ export class VisualizerComponent implements OnInit {
 	xLength = (this.gridX * this.moduleLength) + ((this.gridX - 1) * this.spaceBetween);
 	yLength = (this.gridY * this.moduleLength) + ((this.gridY - 1) * this.spaceBetween);
 
-	xeogl = xeogl;
+	scene: THREE.Scene;
+	camera: THREE.PerspectiveCamera;
+	renderer: THREE.WebGLRenderer;
+	controls: any;
 
 	constructor() { }
 
 	ngOnInit() {
+		this.scene = new THREE.Scene();
+		this.camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+		this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas.nativeElement });
+		this.renderer.gammaInput = true;
+		this.renderer.gammaOutput = true;
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
 		// Resize canvas to fit container
 		const resizeCanvas = () => {
 			const width = this.container.nativeElement.clientWidth;
 			const height = this.container.nativeElement.clientHeight;
 
-			this.canvas.nativeElement.width = width;
-			this.canvas.nativeElement.height = height;
+			this.camera.aspect = width / height;
+			this.camera.updateProjectionMatrix();
+			this.renderer.setSize(width, height);
 		};
 		resizeCanvas();
 		new ResizeSensor(this.container.nativeElement, resizeCanvas);
 
-		// Create scene
-		xeogl.scene = new xeogl.Scene({
-			canvas: this.canvas.nativeElement,
-			backgroundColor: 'red'
-		});
+		// General setup
+		this.scene.background = new THREE.Color('#202020');
 
-		xeogl.scene.camera.view.look = [
-			this.xLength / 2,
-			this.spaceBetween,
-			this.yLength / 2
-		];
+		// Lighting
+		const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+		hemiLight.color.setHSL(0.6, 1, 0.6);
+		hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+		hemiLight.position.set(0, 50, 0);
+		this.scene.add(hemiLight);
+		const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
+		this.scene.add(hemiLightHelper);
 
-		xeogl.scene.camera.view.eye = [
-			-this.xLength,
-			(this.xLength + this.yLength) / 2,
-			-this.yLength
-		];
+		const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+		dirLight.color.setHSL(0.1, 1, 0.95);
+		dirLight.position.set(-1, 1.75, 1);
+		dirLight.position.multiplyScalar(30);
+		this.scene.add(dirLight);
+		dirLight.castShadow = true;
+		dirLight.shadow.mapSize.width = 2048;
+		dirLight.shadow.mapSize.height = 2048;
 
-		// Generate x and y labels
-		// console.log('xeogl', xeogl);
-		// for (let i = 0; i < this.gridX; i++) {
-		// 	new xeogl.Entity({
-		// 		geometry: new xeogl.VectorTextGeometry({
-		// 			text: i,
-		// 			origin: [0, 0, 0],
-		// 			size: 2 // Size of each square character cell
-		// 		}),
-		// 		material: new xeogl.PhongMaterial({
-		// 			emissive: [0.5, 1.0, 1.0],
-		// 			lineWidth: 2
-		// 		}),
-		// 		lights: new xeogl.Lights({
-		// 			lights: [] // No lights - rely on emissive color
-		// 		}),
-		// 		transform: new xeogl.Translate({
-		// 			// xyz: [i * this.spaceBetween, 0, -this.spaceBetween]
-		// 			xyz: [0, 40, 0]
-		// 		})
-		// 	});
-		// }
+		//
 
-		// Generate modules
-		const tipGeometry = new xeogl.BoxGeometry({
-			xSize: this.moduleLength,
-			ySize: this.moduleLength,
-			zSize: this.moduleLength
-		});
-
-		for (let i = 0; i < this.gridX; i++) {
-			this.modules[i] = [];
-
-			for (let j = 0; j < this.gridY; j++) {
-				this.modules[i][j] = new xeogl.Entity({
-					geometry: tipGeometry,
-					material: new xeogl.MetallicMaterial({
-						baseColorMap: new xeogl.Texture({
-							src: '/assets/textures/uvGrid2.jpg'
-						})
-					}),
-					transform: new xeogl.Translate({
-						xyz: [
-							i * this.spaceBetween,
-							0,
-							j * this.spaceBetween
-						]
-					})
-				});
-			}
-		}
-
-		// new xeogl.Entity({
-		// 	geometry: new xeogl.TorusGeometry({
-		// 		radius: 1.0,
-		// 		tube: 0.3,
-		// 		radialSegments: 120,
-		// 		tubeSegments: 60
-		// 	}),
-		// 	material: new xeogl.MetallicMaterial({
-		// 		baseColorMap: new xeogl.Texture({
-		// 			src: '/assets/textures/uvGrid2.jpg'
-		// 		})
-		// 	})
-		// });
-		// xeogl.scene.camera.view.eye = [0, 0, -4];
-		// xeogl.scene.on('tick', function () {
-		// 	this.camera.view.rotateEyeY(0.5);
-		// 	// this.camera.view.rotateEyeX(0.3);
+		// const shiny = new THREE.MeshPhongMaterial({
+		// 	color: 0xeee,
+		// 	specular: 0x050505,
+		// 	shininess: 100
 		// });
 
-		// Allow user camera control
-		new xeogl.InputControl();
+		// Box
+		// const geometry = new THREE.BoxGeometry(1, 1, 1);
+		// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		// const cube = new THREE.Mesh(geometry, shiny);
+		// this.scene.add(cube);
+
+		// material = new THREE.MeshBasicMaterial( {
+		// 	envMap: cubeCamera2.renderTarget.texture
+		// } );
+		// const sphere = new THREE.Mesh( new THREE.IcosahedronBufferGeometry( 20, 3 ) );
+		// this.scene.add( sphere );
+
+		// Sphere
+		const shiny = new THREE.MeshStandardMaterial({ color: '#000', roughness: 1 });
+
+		const shinyEnvMap = new THREE.TextureLoader().load('/assets/textures/shiny-envMap.png');
+		shinyEnvMap.mapping = THREE.SphericalReflectionMapping;
+		shiny.envMap = shinyEnvMap;
+
+		const shinyRoughnessMap = new THREE.TextureLoader().load('/assets/textures/shinyroughnessMap.png');
+		shinyRoughnessMap.magFilter = THREE.NearestFilter;
+		shiny.roughnessMap = shinyRoughnessMap;
+
+		const sphereGeometry = new THREE.SphereGeometry(30, 64, 64);
+
+		const ball = new THREE.Mesh(sphereGeometry, shiny);
+		this.scene.add(ball);
+
+		this.camera.position.z = 5;
+
+		const animate = () => {
+			requestAnimationFrame(animate);
+			this.renderer.render(this.scene, this.camera);
+		};
+		animate();
 	}
 
 }
